654b8793b2397f1be08ce6acab877314dfe3228a
Christopher Kim <chris.kim1990@gmail.com>
2021-02-08 18:48:58 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,30 +6,3 @@ def loop_message_five_times(message)
     count += 1
   end
 end
-
-def loop_message_n_times(message)
-  message = "Hello Moon"
-  count = 0
-  while count limit < do
-    puts message
-    count += 1
-  end
-end
-
-  def output_array(array)
-    array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
-    count = 0
-    while count < array.length do
-      puts array[count]
-      count += 1
-  end
-end
-
-def return_string_array(array)
-  array = [5, 4, 3, 2, 1]
-  count = 0
-  while count < array.length do
-    puts array[count].to_s
-    count += 1
-  end
-end
